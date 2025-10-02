475b6d77989065de736464f2019dc778d6379b3e
frotle <jeffrey.glassford@gmail.com>
2025-10-01 20:11:54 -0400

    get field position from limelight

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/LimeLight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/LimeLight.java
@@ -7,57 +7,55 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.teamcode.RobotContainer;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.LLStatus;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.hardware.PWMOutput;
+
 /** Gyro Subsystem */
 public class LimeLight extends SubsystemBase {
 
     // robot limelight
     private Limelight3A limeLight;
-
+    private Pose2D pose2D;
 
     /** Place code here to initialize subsystem */
     public LimeLight() { // initialize limelight in
         limeLight = RobotContainer.ActiveOpMode.hardwareMap.get(Limelight3A.class, "limeLight");
         limeLight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
         limeLight.start(); // This tells Limelight to start looking
-
     }
 
     /** Method called periodically by the scheduler
      * Place any code here you wish to have run periodically */
     @Override
     public void periodic() {
-
+        // First, tell Limelight which way your robot is facing
+        double robotYaw = RobotContainer.gyro.getYawAngle();
+        limeLight.updateRobotOrientation(robotYaw);
+        LLResult result = limeLight.getLatestResult();
+        if (result != null && result.isValid()) {
+            Pose3D botpose_mt2 = result.getBotpose_MT2();
+            if (botpose_mt2 != null) {
+                double x = botpose_mt2.getPosition().x;
+                double y = botpose_mt2.getPosition().y;
+                pose2D = new Pose2D(DistanceUnit.METER, x,y,AngleUnit.DEGREES,robotYaw);
+            }
+        }
     }
 
     /**
-     * Get gyro angle
+     * Get position from limelight
      *
-     * @return angle in deg between -180 and 180
+     * @return Pose2D
      */
-    public double getYawAngle() {
-        return YawAngle;
-    }
-
-    /**
-     * Resets gyro and offset value
-     */
-    public void resetYawAngle() {
-        setYawAngle(0.0);
-    }
-
-    /**
-     * sets gyro to provided angle (in deg)
-     *
-     * @param angle an angle in degrees
-     */
-    public void setYawAngle(double angle) {
-
-        YawAngleOffset -= getYawAngle() - angle;
+    public Pose2D getFieldPosition() {
+        return pose2D;
     }
 
 }
