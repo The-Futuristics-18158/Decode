7ed7f870b756e2090d9c794420a651db20705df4
superzokabear <superzokabear@gmail.com>
2025-11-17 11:01:23 -0500

    Intake power updated to 1.0.
    CLimb subsystem and target height systems. Still needs tuning and other values.It is still on the numbers from last year. Not in robot container so no worries about hitting the wrong button.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ClimbSubsystem.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import org.firstinspires.ftc.teamcode.RobotContainer;
+
+
+/**
+ * Linear Slide Subsystem
+ */
+public class ClimbSubsystem extends SubsystemBase {
+
+    // Initialize both motors
+    private final DcMotorEx climb;
+    ;
+
+
+    /**
+     * Place code here to initialize subsystem
+     */
+    public ClimbSubsystem() {
+
+        // Creates the motors using the hardware map
+        climb = RobotContainer.ActiveOpMode.hardwareMap.get(DcMotorEx.class, "climb");
+
+
+        // Resets the encoders for both motors
+        climb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+
+        // Turn the left motor in reverse to move the slide upwards
+        climb.setDirection(DcMotorSimple.Direction.FORWARD);
+
+
+        // Sets the motors PIDF values
+        climb.setVelocityPIDFCoefficients(10.0, 0.2, 0.001, 10.0);
+
+
+        // Setting target to zero upon initialization
+        climb.setTargetPosition(0);
+
+        // Puts the motors into position control mode
+        climb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+    }
+
+    /**
+     * Method called periodically by the scheduler
+     * Place any code here you wish to have run periodically
+     */
+    @Override
+    public void periodic() {
+
+        //RobotContainer.DBTelemetry.addData("ClimbPose ", climb.getCurrentPosition());
+        //RobotContainer.DBTelemetry.update();
+
+    }
+
+    // Using the var ticks sets the motor encoder ticks to a set position
+    public void moveClimb(int ticks) {
+
+        // Sets both motors to the ticks target position
+        climb.setTargetPosition(ticks);
+
+        // Sets the power VERY IMPORTANT
+        climb.setPower(1);
+
+    }
+
+    public void moveTo(ClimbTargetHeight target) {moveClimb(target.getValue());}
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/IntakeSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/IntakeSubsystem.java
@@ -42,7 +42,7 @@ public class IntakeSubsystem extends SubsystemBase {
 
     // Run the intake at set percentage of motor voltage
     public void intakeRun(){
-        intakeMotor.setPower(0.75);
+        intakeMotor.setPower(1.0);
     }
     // Stop intake
     public void intakeStop(){
