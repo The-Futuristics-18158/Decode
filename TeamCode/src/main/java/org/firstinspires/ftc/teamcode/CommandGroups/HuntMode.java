3d745cf8570118b57340ea6c288d8948a9fb4320
superzokabear <superzokabear@gmail.com>
2025-11-20 21:19:21 -0500

    notes for layout of these automated functions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandGroups/HuntMode.java
@@ -0,0 +1,97 @@
+package org.firstinspires.ftc.teamcode.CommandGroups;
+
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+
+// Example Sequential Command Group
+// There are also:
+// ParallelCommandGroup
+// ParallelRaceGroup
+// ParallelDeadlineGroup
+
+public class HuntMode extends SequentialCommandGroup {
+
+    // constructor
+    public HuntMode() {
+
+        addCommands (
+                //button tells if were looking for a purple ball or a green-
+                //-(maybe should have a hunt green and a hunt purple command group?
+                // definitely a grab whatever is closest too
+                // allow manual drive to turn robot to the general direction of a ball
+                // webcam looks for ball
+                // turn to ball ... this used a PID control on the x offset (camera)
+                // turn on intake
+                // when detected drive to nearest ball
+                // turn off intake when you have a ball
+        );
+    }
+
+}
+
+// Example #1: Lily's 2023 FRC super cube auto
+/*          // enable arm, and lift to stow position
+            new InstantCommand(() -> RobotContainer.arm.SetEnableArm(true)),
+
+            // move arm back to drop off cube
+            new InstantCommand(() -> RobotContainer.arm.SetArmPosition(RobotContainer.arm.HIGH_DEG)),
+
+            // delay until arm gets back
+            new DelayCommand(1.0),
+
+            // place cube
+            new InstantCommand(() -> RobotContainer.grabber.setClose()),
+
+            // delay for gripper to close
+            new DelayCommand(0.7),
+
+            // move arm to 'forward position' but inside robot bumper)
+            // move to 135deg
+            new InstantCommand(() -> RobotContainer.arm.SetArmPosition(135.0)),
+
+            // delay for arm to get to stow
+            new DelayCommand(1.5),
+
+            // ensure arm is stowed before it is allow to begin moving over charge station
+            new SafetyCheckStowPosition(),
+
+            // drive right
+            // new DrivetoRelativePose(new Pose2d(0,-2.0, new Rotation2d(0.0)), 1.0, 0.1, 5.0),
+
+            // drive straight
+            new DrivetoRelativePose(new Pose2d(5.0, 0, new Rotation2d(0.0)),1.8,0.1, 7.0),
+
+            // pick up cube from floor :)
+            new AutoFloorCubePickup(),
+
+            // delay
+            new DelayCommand(0.5),
+
+            // drive back
+            //new DrivetoRelativePose(new Pose2d(1.0,0, new Rotation2d(0.0)), 1.0, 0.1, 2.0),
+
+            // drive left to center
+            new DrivetoRelativePose(new Pose2d(-1.0,2.0, new Rotation2d(0.0)), 1.8, 0.1, 5.0),
+
+            // drive straight onto charge station
+            new DrivetoRelativePose(new Pose2d(-1.5, 0, new Rotation2d(0.0)),1.0,0.1, 30.0),
+
+            // balance
+            new AutoBalance()
+
+// Example #2: Matthew's 2024 shoot donut sequence.
+This sequence contains parallel and parallelrace subgroups within an overall series command
+
+      addCommands(
+      new ParallelRaceGroup(
+        new AimToSpeaker(),
+        new SpinupSpeaker()
+      ),
+      new ParallelCommandGroup(
+        new WaitForEffectorAngle(),
+        new WaitForShooterSpinup()
+      ),
+
+      new ShootSpeaker()
+    );
+
+ */
