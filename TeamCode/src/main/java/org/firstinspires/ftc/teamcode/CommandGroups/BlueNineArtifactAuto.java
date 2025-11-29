1a22e3d4c97a69987ffab539a8c414ff99215428
superzokabear <superzokabear@gmail.com>
2025-11-29 16:28:14 -0500

    Blue Nine Artifact Auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandGroups/BlueNineArtifactAuto.java
@@ -0,0 +1,199 @@
+package org.firstinspires.ftc.teamcode.CommandGroups;
+
+import com.arcrobotics.ftclib.command.ParallelRaceGroup;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.geometry.Pose2d;
+import com.arcrobotics.ftclib.geometry.Rotation2d;
+import com.arcrobotics.ftclib.purepursuit.waypoints.PointTurnWaypoint;
+
+import org.firstinspires.ftc.teamcode.CommandGroups.Shoot.ShootAllObeliskColor;
+import org.firstinspires.ftc.teamcode.Commands.Drive.MoveToPose;
+import org.firstinspires.ftc.teamcode.Commands.Intake.IntakeCommand;
+import org.firstinspires.ftc.teamcode.Utility.AutoFunctions;
+
+// Example Sequential Command Group
+// There are also:
+// ParallelCommandGroup
+// ParallelRaceGroup
+// ParallelDeadlineGroup
+
+public class BlueNineArtifactAuto extends SequentialCommandGroup {
+
+    // constructor
+    public BlueNineArtifactAuto() {
+
+        addCommands (
+                //move to a shooting position
+                new MoveToPose(
+                        0.5,
+                        0.5,
+                        AutoFunctions.redVsBlue((new Pose2d(1.66, -0.3, new Rotation2d(Math.toRadians(-150.0)))))), // + or - 20 degrees
+
+                new ShootAllObeliskColor(),
+
+                //move to intake point
+                new MoveToPose(
+                        0.5,
+                        0.5,
+                        AutoFunctions.redVsBlue((new Pose2d(1.0, -0.7, new Rotation2d(Math.toRadians(-90.0)))))),
+
+                // intaking and moving forwards
+                new ParallelRaceGroup(
+                        new IntakeCommand(),
+                        // move to pickup
+                        new MoveToPose(
+                                0.5,
+                                0.5,
+                                AutoFunctions.redVsBlue((new Pose2d(1, -1.4, new Rotation2d(Math.toRadians(180.0))))))
+                ),
+
+                // move to shoot
+                new MoveToPose(
+                        0.5,
+                        0.5,
+                        AutoFunctions.redVsBlue((new Pose2d(1.66, -0.3, new Rotation2d(Math.toRadians(-90.0)))))),
+
+                new ShootAllObeliskColor(),
+
+                // move to pickup
+                new MoveToPose(
+                        0.5,
+                        0.5,
+                        AutoFunctions.redVsBlue((new Pose2d(0.33, -0.7, new Rotation2d(Math.toRadians(-90.0)))))),
+
+                // intaking and moving forwards
+                new ParallelRaceGroup(
+                        new IntakeCommand(),
+                        // move to pickup
+                        new MoveToPose(
+                                0.5,
+                                0.5,
+                                AutoFunctions.redVsBlue((new Pose2d(0.33, -1.4, new Rotation2d(Math.toRadians(180.0))))))
+                ),
+
+
+                new MoveToPose(
+                        0.5,
+                        0.5,
+                        AutoFunctions.redVsBlue((new Pose2d(-0.6, -0.6, new Rotation2d(Math.toRadians(-90.0)))))),
+
+                new ShootAllObeliskColor(),
+
+                new MoveToPose(
+                        0.5,
+                        0.5,
+                        AutoFunctions.redVsBlue((new Pose2d(0, 0.7, new Rotation2d(Math.toRadians(-90.0))))))
+
+        );
+    }
+}
+
+//Example MoveToPose
+// new MoveToPose(
+//           1.5,
+//           1.0,
+// AutoFunctions.redVsBlue(new Pose2d(-0.22, 1.2, new Rotation2d(Math.toRadians(-90))))
+//        ),
+
+//Example FollowPath
+// new FollowPath(
+//             2.0,
+//             1.0,
+//             0.0,
+//             0.0,
+// AutoFunctions.redVsBlue(new Rotation2d(Math.toRadians(-90.0))),
+// new ArrayList<Translation2d>() {{ }},
+// AutoFunctions.redVsBlue(new Pose2d(0.55, 0.25, new Rotation2d(Math.toRadians(-180)))),
+// AutoFunctions.redVsBlue(new Rotation2d(Math.toRadians(-180)))),
+
+
+// Example #2: Using conditional command
+// conditionally run commands depending on condition
+/*      new FourWayConditionalCommand(
+            ()-> { return true; },
+            new SequentialCommandGroup(
+                commands to do this
+            ),
+            ()-> { return false; },
+            new SequentialCommandGroup(
+                commands to do that
+            ),
+            ()-> { return false; },
+            new SequentialCommandGroup(
+                commands to do those
+            ),
+            new SequentialCommandGroup(
+                commands to do something else
+            )
+
+        ) // end FourWayCondition
+*/
+
+
+// Example #1: Lily's 2023 FRC super cube auto
+/*          // enable arm, and lift to stow position
+            new InstantCommand(() -> RobotContainer.arm.SetEnableArm(true)),
+
+            // move arm back to drop off cube
+            new InstantCommand(() -> RobotContainer.arm.SetArmPosition(RobotContainer.arm.HIGH_DEG)),
+
+            // delay until arm gets back
+            new DelayCommand(1.0),
+
+            // place cube
+            new InstantCommand(() -> RobotContainer.grabber.setClose()),
+
+            // delay for gripper to close
+            new DelayCommand(0.7),
+
+            // move arm to 'forward position' but inside robot bumper)
+            // move to 135deg
+            new InstantCommand(() -> RobotContainer.arm.SetArmPosition(135.0)),
+
+            // delay for arm to get to stow
+            new DelayCommand(1.5),
+
+            // ensure arm is stowed before it is allow to begin moving over charge station
+            new SafetyCheckStowPosition(),
+
+            // drive right
+            // new DrivetoRelativePose(new Pose2d(0,-2.0, new Rotation2d(0.0)), 1.0, 0.1, 5.0),
+
+            // drive straight
+            new DrivetoRelativePose(new Pose2d(5.0, 0, new Rotation2d(0.0)),1.8,0.1, 7.0),
+
+            // pick up cube from floor :)
+            new AutoFloorCubePickup(),
+
+            // delay
+            new DelayCommand(0.5),
+
+            // drive back
+            //new DrivetoRelativePose(new Pose2d(1.0,0, new Rotation2d(0.0)), 1.0, 0.1, 2.0),
+
+            // drive left to center
+            new DrivetoRelativePose(new Pose2d(-1.0,2.0, new Rotation2d(0.0)), 1.8, 0.1, 5.0),
+
+            // drive straight onto charge station
+            new DrivetoRelativePose(new Pose2d(-1.5, 0, new Rotation2d(0.0)),1.0,0.1, 30.0),
+
+            // balance
+            new AutoBalance()
+
+// Example #2: Matthew's 2024 shoot donut sequence.
+This sequence contains parallel and parallelrace subgroups within an overall series command
+
+      addCommands(
+      new ParallelRaceGroup(
+        new AimToSpeaker(),
+        new SpinupSpeaker()
+      ),
+      new ParallelCommandGroup(
+        new WaitForEffectorAngle(),
+        new WaitForShooterSpinup()
+      ),
+
+      new ShootSpeaker()
+    );
+
+ */
