68e01695c491d9094363399c0d4947c586235135
superzokabear <superzokabear@gmail.com>
2025-11-29 16:21:21 -0500

    button fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/Intake/AimToShoot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/Intake/AimToShoot.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.Commands.Intake;
 
 import com.arcrobotics.ftclib.command.CommandBase;
 import com.arcrobotics.ftclib.controller.PIDController;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
 
 import org.firstinspires.ftc.teamcode.RobotContainer;
 import org.firstinspires.ftc.vision.opencv.ColorBlobLocatorProcessor;
@@ -9,23 +10,24 @@ import org.firstinspires.ftc.vision.opencv.ColorBlobLocatorProcessor;
 import java.util.List;
 
 public class AimToShoot extends CommandBase {
-    private boolean haveArtifact;
-    private double blobX;
+    private boolean haveTarget;
+    private double TargetX;
     PIDController omegaControl;
+    double OnTargetTime;
 
     // constructor
     public AimToShoot() {
-        haveArtifact = false;
+        haveTarget = false;
         // add subsystem requirements (if any) - for example:
         addRequirements(RobotContainer.drivesystem);
-        omegaControl = new PIDController(0.015, 0.0, 0.0);
+        omegaControl = new PIDController(0.01, 0.0005, 0.0);
+        OnTargetTime = 0;
     }
 
     // This method is called once when command is started
     @Override
     public void initialize() {
         omegaControl.reset();
-        RobotContainer.intake.intakeRun();
     }
 
     // This method is called periodically while command is active
@@ -36,23 +38,29 @@ public class AimToShoot extends CommandBase {
         double y_speed;
         double omega_speed;
 
-        List<ColorBlobLocatorProcessor.Blob> blobs;
-        blobs = RobotContainer.rampCamera.GetAllBlobDetections();
+        LLResultTypes.FiducialResult target = RobotContainer.limeLight.getTargetInfo();
 
-        if (blobs != null && !(blobs.isEmpty())) {
-            haveArtifact = true;
-            blobX = blobs.get(0).getCircle().getCenter().x - 160.0;
+
+        if (target != null) {
+            haveTarget = true;
+            TargetX = target.getTargetXDegrees();
 
             // determine sideways speed
-            omega_speed = omegaControl.calculate(blobX); //320
+            omega_speed = omegaControl.calculate(TargetX); //320
         } else {
-            haveArtifact = false;
-            blobX = 0;
+            haveTarget = false;
+            TargetX = 0;
             omega_speed = 0.0;
         }
 
-        x_speed = 0.9;//was 0.20
+        x_speed = 0.0;
         y_speed= 0;
+
+        if (haveTarget == true && Math.abs(TargetX)<0.5){
+            OnTargetTime += 0.02;
+        }else{
+            OnTargetTime = 0.0;
+        }
 
         RobotContainer.drivesystem.RobotDrive(x_speed, y_speed, omega_speed);
 
@@ -61,16 +69,13 @@ public class AimToShoot extends CommandBase {
     @Override
     public boolean isFinished(){
 
-        return false;
+        return OnTargetTime >= 0.25;
     }
 
     // This method is called once when command is finished.
     @Override
     public void end(boolean interrupted) {
-
         RobotContainer.drivesystem.RobotDrive(0,0,0);
-        RobotContainer.intake.intakeStop();
-
     }
 
 }
