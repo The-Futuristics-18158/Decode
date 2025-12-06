0ce7d3369aaa9d3437d55a2fe639d249018bdef2
frotle <jeffrey.glassford@gmail.com>
2025-12-06 16:45:51 -0500

    blinkin 2.0

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Blinkin.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Blinkin.java
@@ -33,9 +33,9 @@ public class Blinkin extends SubsystemBase {
     public void periodic() {
         if(RobotContainer.colour.isLeftArtifactPresent()|| RobotContainer.colour.isRightArtifactPresent()){
             ShowBallColours();
-//            if (RobotContainer.limeLight.hasGoal){
-//                ShowHasGoal();
-//            }
+            if (RobotContainer.limeLight.hasGoal){
+                ShowHasGoal();
+            }
         } else{
            ShowAlliance();
        }
@@ -74,7 +74,15 @@ public class Blinkin extends SubsystemBase {
                blinkin.setPattern(RevBlinkinLedDriver.BlinkinPattern.VIOLET);
            }
            if (timer.seconds() >=1.5){
-               timer.reset();
+               if (RobotContainer.limeLight.hasGoal){
+                   blinkin.setPattern(RevBlinkinLedDriver.BlinkinPattern.WHITE);
+                   if(timer.seconds() >=2){
+                       timer.reset();
+                   }
+               }else{
+                   timer.reset();
+               }
+
            }
        }else if (hasPurple){
            blinkin.setPattern(RevBlinkinLedDriver.BlinkinPattern.VIOLET);
