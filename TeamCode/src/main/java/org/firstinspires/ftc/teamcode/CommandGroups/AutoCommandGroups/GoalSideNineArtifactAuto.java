08e2e13532a28e409eb0cdb01dde56885c05dc98
superzokabear <superzokabear@gmail.com>
2026-01-29 23:11:39 -0500

    cleaning up comments and adding in new features to old autos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandGroups/AutoCommandGroups/GoalSideNineArtifactAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandGroups/AutoCommandGroups/GoalSideNineArtifactAuto.java
@@ -19,59 +19,55 @@ import org.firstinspires.ftc.teamcode.Utility.AutoFunctions;
 
 import java.util.ArrayList;
 
-// Example Sequential Command Group
-// There are also:
-// ParallelCommandGroup
-// ParallelRaceGroup
-// ParallelDeadlineGroup
-
 public class GoalSideNineArtifactAuto extends SequentialCommandGroup {
 
-    // constructor
+    // Constructor
     public GoalSideNineArtifactAuto() {
 
         addCommands (
 
                 new InstantCommand(()-> RobotContainer.odometry.setCurrentPos(AutoFunctions.redVsBlue(new Pose2d(-1.29, -1.29, new Rotation2d(Math.toRadians(-45.0)))))), // was x = -1.2, y = -1.35
-                //move to a shooting position
+
+                // Move to a shot #1
                 new MoveToPose(
                         1.5,
                         0.7,
                         AutoFunctions.redVsBlue((new Pose2d(-0.45, -0.45, new Rotation2d(Math.toRadians(45.0)))))),// + or - 20 degrees // was -0.6, -0.6
 
+//      -------------------------- Artifact Cycle #1 --------------------------
                 new FastShootObeliskColor(),
 
+                // Turn to Hunt
                 new TurnTo(AutoFunctions.redVsBlue(-90.0),false,1.5),
 
-                // intaking and moving forwards
+                // Hunt
                 new HuntModeCommand(2.5),
 
+                // Clean-up Hunt
                 new JogBackIntake(),
 
+                // Move to shot #2
                 new MoveToPose(
                         1.5,
                         0.8,
                         AutoFunctions.redVsBlue((new Pose2d(-0.6, -0.6, new Rotation2d(Math.toRadians(45.0)))))),
 
-
+//      -------------------------- Artifact Cycle #2 --------------------------
                 new FastShootObeliskColor(),
 
-//              move to pickup
+                // Move to pickup
                 new MoveToPose(
                         1.5,
                         0.7,
                         AutoFunctions.redVsBlue((new Pose2d(0.3, -0.6, new Rotation2d(Math.toRadians(-90.0)))))),
 
-                // intaking and moving forwards
+                // Hunt
                 new HuntModeCommand(2.5),
 
+                // Clean-up Hunt
                 new JogBackIntake(),
 
-//                new MoveToPose(
-//                        1.5,
-//                        0.7,
-//                        AutoFunctions.redVsBlue((new Pose2d(-0.6, -0.6, new Rotation2d(Math.toRadians(45.0)))))),
-
+                // Follow a path around the gate to shot #3
                 new FollowPath(
                         1.5,
                         0.7,
@@ -82,9 +78,10 @@ public class GoalSideNineArtifactAuto extends SequentialCommandGroup {
                         AutoFunctions.redVsBlue(new Pose2d(-0.6, -0.6, new Rotation2d(Math.toRadians(150.0)))),
                         AutoFunctions.redVsBlue(new Rotation2d(Math.toRadians(45)))),
 
-
+//      -------------------------- Artifact Cycle #3 --------------------------
                 new FastShootObeliskColor(),
 
+                // Move off line
                 new MoveToPose(
                         1.5,
                         0.7,
@@ -134,72 +131,3 @@ public class GoalSideNineArtifactAuto extends SequentialCommandGroup {
 
         ) // end FourWayCondition
 */
-
-
-// Example #1: Lily's 2023 FRC super cube auto
-/*          // enable arm, and lift to stow position
-            new InstantCommand(() -> RobotContainer.arm.SetEnableArm(true)),
-
-            // move arm back to drop off cube
-            new InstantCommand(() -> RobotContainer.arm.SetArmPosition(RobotContainer.arm.HIGH_DEG)),
-
-            // delay until arm gets back
-            new DelayCommand(1.0),
-
-            // place cube
-            new InstantCommand(() -> RobotContainer.grabber.setClose()),
-
-            // delay for gripper to close
-            new DelayCommand(0.7),
-
-            // move arm to 'forward position' but inside robot bumper)
-            // move to 135deg
-            new InstantCommand(() -> RobotContainer.arm.SetArmPosition(135.0)),
-
-            // delay for arm to get to stow
-            new DelayCommand(1.5),
-
-            // ensure arm is stowed before it is allow to begin moving over charge station
-            new SafetyCheckStowPosition(),
-
-            // drive right
-            // new DrivetoRelativePose(new Pose2d(0,-2.0, new Rotation2d(0.0)), 1.0, 0.1, 5.0),
-
-            // drive straight
-            new DrivetoRelativePose(new Pose2d(5.0, 0, new Rotation2d(0.0)),1.8,0.1, 7.0),
-
-            // pick up cube from floor :)
-            new AutoFloorCubePickup(),
-
-            // delay
-            new DelayCommand(0.5),
-
-            // drive back
-            //new DrivetoRelativePose(new Pose2d(1.0,0, new Rotation2d(0.0)), 1.0, 0.1, 2.0),
-
-            // drive left to center
-            new DrivetoRelativePose(new Pose2d(-1.0,2.0, new Rotation2d(0.0)), 1.8, 0.1, 5.0),
-
-            // drive straight onto charge station
-            new DrivetoRelativePose(new Pose2d(-1.5, 0, new Rotation2d(0.0)),1.0,0.1, 30.0),
-
-            // balance
-            new AutoBalance()
-
-// Example #2: Matthew's 2024 shoot donut sequence.
-This sequence contains parallel and parallelrace subgroups within an overall series command
-
-      addCommands(
-      new ParallelRaceGroup(
-        new AimToSpeaker(),
-        new SpinupSpeaker()
-      ),
-      new ParallelCommandGroup(
-        new WaitForEffectorAngle(),
-        new WaitForShooterSpinup()
-      ),
-
-      new ShootSpeaker()
-    );
-
- */
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandGroups/AutoCommandGroups/LoadingSideNineArtifactAutoDeluxe.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandGroups/AutoCommandGroups/LoadingSideNineArtifactAutoDeluxe.java
@@ -6,46 +6,47 @@ import com.arcrobotics.ftclib.geometry.Pose2d;
 import com.arcrobotics.ftclib.geometry.Rotation2d;
 import com.arcrobotics.ftclib.geometry.Translation2d;
 
+import org.firstinspires.ftc.teamcode.CommandGroups.Shoot.FastShootObeliskColor;
 import org.firstinspires.ftc.teamcode.CommandGroups.Shoot.ShootAllObeliskColor;
 import org.firstinspires.ftc.teamcode.Commands.Drive.FollowPath;
 import org.firstinspires.ftc.teamcode.Commands.Drive.MoveToPose;
 import org.firstinspires.ftc.teamcode.Commands.Intake.HuntModeCommand;
+import org.firstinspires.ftc.teamcode.Commands.Intake.JogBackIntake;
 import org.firstinspires.ftc.teamcode.RobotContainer;
 import org.firstinspires.ftc.teamcode.Utility.AutoFunctions;
 
 import java.util.ArrayList;
 
-// Example Sequential Command Group
-// There are also:
-// ParallelCommandGroup
-// ParallelRaceGroup
-// ParallelDeadlineGroup
-
 public class LoadingSideNineArtifactAutoDeluxe extends SequentialCommandGroup {
 
-    // constructor
+    // Constructor
     public LoadingSideNineArtifactAutoDeluxe() {
 
         addCommands (
-                // was X = 1.59 Y was -0.39
+                // Was X = 1.59 Y was -0.39
                 new InstantCommand(()-> RobotContainer.odometry.setCurrentPos(AutoFunctions.redVsBlue(new Pose2d(1.64, -0.35, new Rotation2d(Math.toRadians(0.0)))))),
-                //move to a shooting position
+
+                // Move to shot #1
                 new MoveToPose(
                         1.75,
                        1.0,
                         AutoFunctions.redVsBlue((new Pose2d(-0.3, -0.3, new Rotation2d(Math.toRadians(45.0)))))), // + or - 20 degrees
 
-                new ShootAllObeliskColor(),
+//      -------------------------- Artifact Cycle #1 --------------------------
+                new FastShootObeliskColor(),
 
-                //move to intake point
+                // Move to pickup
                 new MoveToPose(
                         1.5,
                         1.5,
                         AutoFunctions.redVsBlue((new Pose2d(0.9, -0.6, new Rotation2d(Math.toRadians(-90.0)))))),
 
-                // intaking and moving forwards
-                new HuntModeCommand(4.0),
+                // Hunt
+                new HuntModeCommand(2.5),
+                // Clean-up Hunt
+                new JogBackIntake(),
 
+                // Follow a path around the gate to shot #2
                 new FollowPath(
                         1.5,
                         1.0,
@@ -56,25 +57,28 @@ public class LoadingSideNineArtifactAutoDeluxe extends SequentialCommandGroup {
                         AutoFunctions.redVsBlue(new Pose2d(-0.3, -0.3, new Rotation2d(Math.toRadians(180)))),
                         AutoFunctions.redVsBlue(new Rotation2d(Math.toRadians(45.0)))),
 
-                new ShootAllObeliskColor(),
+//      -------------------------- Artifact Cycle #2 --------------------------
+                new FastShootObeliskColor(),
 
-                // move to pickup
+                // Move to pickup
                 new MoveToPose(
                         1.5,
                         1.0,
                         AutoFunctions.redVsBlue((new Pose2d(0.3, -0.6, new Rotation2d(Math.toRadians(-90.0)))))),
 
-                // intaking and moving forwards
-                new HuntModeCommand(4.0),
-
-
+                // Hunt
+                new HuntModeCommand(2.5),
+                // Clean-up Hunt
+                new JogBackIntake(),
+                // Move to shot #3
                 new MoveToPose(
                         1.5,
                         1.0,
                         AutoFunctions.redVsBlue((new Pose2d(-0.3, -0.3, new Rotation2d(Math.toRadians(45.0)))))),
 
-                new ShootAllObeliskColor(),
-
+//      -------------------------- Artifact Cycle #3 --------------------------
+                new FastShootObeliskColor(),
+                // Move off line
                 new MoveToPose(
                         1.5,
                         1.5,
@@ -124,72 +128,3 @@ public class LoadingSideNineArtifactAutoDeluxe extends SequentialCommandGroup {
 
         ) // end FourWayCondition
 */
-
-
-// Example #1: Lily's 2023 FRC super cube auto
-/*          // enable arm, and lift to stow position
-            new InstantCommand(() -> RobotContainer.arm.SetEnableArm(true)),
-
-            // move arm back to drop off cube
-            new InstantCommand(() -> RobotContainer.arm.SetArmPosition(RobotContainer.arm.HIGH_DEG)),
-
-            // delay until arm gets back
-            new DelayCommand(1.0),
-
-            // place cube
-            new InstantCommand(() -> RobotContainer.grabber.setClose()),
-
-            // delay for gripper to close
-            new DelayCommand(0.7),
-
-            // move arm to 'forward position' but inside robot bumper)
-            // move to 135deg
-            new InstantCommand(() -> RobotContainer.arm.SetArmPosition(135.0)),
-
-            // delay for arm to get to stow
-            new DelayCommand(1.5),
-
-            // ensure arm is stowed before it is allow to begin moving over charge station
-            new SafetyCheckStowPosition(),
-
-            // drive right
-            // new DrivetoRelativePose(new Pose2d(0,-2.0, new Rotation2d(0.0)), 1.0, 0.1, 5.0),
-
-            // drive straight
-            new DrivetoRelativePose(new Pose2d(5.0, 0, new Rotation2d(0.0)),1.8,0.1, 7.0),
-
-            // pick up cube from floor :)
-            new AutoFloorCubePickup(),
-
-            // delay
-            new DelayCommand(0.5),
-
-            // drive back
-            //new DrivetoRelativePose(new Pose2d(1.0,0, new Rotation2d(0.0)), 1.0, 0.1, 2.0),
-
-            // drive left to center
-            new DrivetoRelativePose(new Pose2d(-1.0,2.0, new Rotation2d(0.0)), 1.8, 0.1, 5.0),
-
-            // drive straight onto charge station
-            new DrivetoRelativePose(new Pose2d(-1.5, 0, new Rotation2d(0.0)),1.0,0.1, 30.0),
-
-            // balance
-            new AutoBalance()
-
-// Example #2: Matthew's 2024 shoot donut sequence.
-This sequence contains parallel and parallelrace subgroups within an overall series command
-
-      addCommands(
-      new ParallelRaceGroup(
-        new AimToSpeaker(),
-        new SpinupSpeaker()
-      ),
-      new ParallelCommandGroup(
-        new WaitForEffectorAngle(),
-        new WaitForShooterSpinup()
-      ),
-
-      new ShootSpeaker()
-    );
-
- */
