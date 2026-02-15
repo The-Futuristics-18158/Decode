38e7c3c1b03cdf2f0ae7f4aac9eff0d03f247b46
knutt5 <44103178+knutt5@users.noreply.github.com>
2026-02-15 17:34:09 -0500

    update

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/Intake/JogBackIntakeFull.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.Commands.Intake;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+import org.firstinspires.ftc.teamcode.RobotContainer;
+
+
+// command template
+public class JogBackIntakeFull extends CommandBase {
+
+    // constructor
+    public JogBackIntakeFull() {
+
+        // add subsystem requirements (if any) - for example:
+        addRequirements(RobotContainer.intake);
+
+    }
+
+    // This method is called once when command is started
+    @Override
+    public void initialize() {
+
+        RobotContainer.intake.intakeReverse();
+    }
+
+    // This method is called periodically while command is active
+    @Override
+    public void execute() {
+
+    }
+
+    // This method to return true only when command is to finish. Otherwise return false
+    @Override
+    public boolean isFinished() {
+
+
+    }
+
+    // This method is called once when command is finished.
+    @Override
+    public void end(boolean interrupted) {
+
+        RobotContainer.intake.intakeStop();
+    }
+
+}
