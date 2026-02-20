66a09b9199efe452a561af0d8c7ceec3078c206c
knutt5 <44103178+knutt5@users.noreply.github.com>
2026-02-19 22:38:17 -0500

    Add new hunt mode auto command

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/Intake/HuntMode/HuntModeAutoZeroAngle.java
@@ -0,0 +1,134 @@
+package org.firstinspires.ftc.teamcode.Commands.Intake.HuntMode;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.RobotContainer;
+import org.firstinspires.ftc.teamcode.Utility.Utils;
+import org.firstinspires.ftc.vision.opencv.ColorBlobLocatorProcessor;
+
+import java.util.List;
+
+public class HuntModeAutoZeroAngle extends CommandBase {
+    private boolean haveArtifact;
+    private double blobX;
+    PIDController omegaControl;
+    PIDController xControl;
+    int finishedCounter;
+    private ElapsedTime timer;
+    private double seconds;
+    private double targetAngle;
+
+    // constructor
+    public HuntModeAutoZeroAngle(){
+        this(5.0);
+    }
+    public HuntModeAutoZeroAngle(double time) {
+        haveArtifact = false;
+
+        // add subsystem requirements (if any) - for example:
+        addRequirements(RobotContainer.drivesystem);
+        addRequirements(RobotContainer.intake);
+
+        omegaControl = new PIDController(0.05, 0.0005, 0.01);
+        xControl = new PIDController(0.0075, 0.0, 0.0);
+        timer = new ElapsedTime();
+        seconds = time;
+    }
+
+    // This method is called once when command is started
+    @Override
+    public void initialize() {
+        omegaControl.reset();
+        xControl.reset();
+        //finished = false;
+        finishedCounter=0;
+        timer.reset();
+        targetAngle = 0.0;
+    }
+
+    // This method is called periodically while command is active
+    @Override
+    public void execute() {
+        // gets list of blobs from the camera
+        double x_speed;
+        double y_speed;
+        double omega_speed;
+
+        if (timer.seconds() >= seconds || (RobotContainer.colour.isLeftArtifactPresent() &&
+                RobotContainer.colour.isRightArtifactPresent()
+                && RobotContainer.distance.isRampArtifactPresent())){
+
+            RobotContainer.intake.intakeStop();
+            finishedCounter++;
+            //finished = true;
+
+        }else {
+            finishedCounter=0;
+            if (RobotContainer.colour.isLeftArtifactPresent() && RobotContainer.colour.isRightArtifactPresent())
+                RobotContainer.intake.intakeRunReducedSpeed();
+            else
+                RobotContainer.intake.intakeRun();
+        }
+
+        double angleError = Utils.AngleDifference(RobotContainer.gyro.getYawAngle(),targetAngle);
+        omega_speed = -omegaControl.calculate(angleError);
+
+        List<ColorBlobLocatorProcessor.Blob> blobs;
+        // change this later if for get green blob detections and or get purple blob detections (if we want to look for one specific color)
+        blobs = RobotContainer.rampCamera.GetAllBlobDetections();
+
+        // If the robot sees an artifact in the camera
+        if (blobs != null && !(blobs.isEmpty())) {
+            haveArtifact = true;
+            blobX = blobs.get(0).getCircle().getCenter().x - 160.0;
+
+            // set forward speed to value depending on how far artifact from center of camera
+            // first number is forward
+            // second number is how quickly the speed goes down when artifact is off center
+            y_speed = 0.6 - 0.5*Math.min(Math.abs(blobX/160.0),1.0);
+
+            if (y_speed < 0.0){ y_speed = 0.0;}
+
+
+            x_speed = xControl.calculate(blobX);
+
+        // Don't see anything therefor go ahead, no strafe
+        } else {
+            haveArtifact = false;
+            blobX = 0;
+            //omega_speed = 0.0;
+            y_speed = 0.8;
+            x_speed = 0.0;
+        }
+
+       // if busy intaking robot, stop and wait for it to finish before proceeding
+        if (RobotContainer.distance.isRampArtifactPresent()) {
+            y_speed = 0.0;
+            x_speed = 0.0;
+        }
+
+        // NOTE: for 0-angle hunt, swap x and y angles
+        RobotContainer.drivesystem.FieldDrive(y_speed, x_speed, omega_speed);
+
+    }
+    // This method to return true only when command is to finish. Otherwise return false
+    @Override
+    public boolean isFinished(){
+
+        return (finishedCounter>=2);
+
+        //return finished;
+    }
+
+    // This method is called once when command is finished.
+    @Override
+    public void end(boolean interrupted) {
+
+        RobotContainer.drivesystem.RobotDrive(0,0,0);
+        RobotContainer.intake.intakeStop();
+
+    }
+
+}
